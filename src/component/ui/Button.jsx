a5ff11b2b72947fdd34cc20c5c3f2828f130092e
ash89tiger <153449354+ash89tiger@users.noreply.github.com>
2024-06-04 23:44:55 +0900

    Update Button.jsx
    edit Button.jsx/props=>background,color,height,width,title

--- src/component/ui/Button.jsx
+++ src/component/ui/Button.jsx
@@ -1,1 +1,31 @@
+import React from "react";
+import styled from "styled-components";
 
+const StyledButton = styled.button`
+    background-color: ${(props) => props.$background || "#3182F7"};
+    height: ${(props) => props.$height || "auto"};
+    width: ${(props) => props.$width || "auto"};
+    color: ${(props) => props.$color || "#fff"};;
+    border: none;
+    font-size:16px;
+    padding: 12px;
+    border-radius:20px;
+`
+
+function Button(props){
+
+    const { background, color, height, width, title, onClick } = props;
+
+    return (
+        <StyledButton 
+        $background={background}
+        $color={color}
+        $height={height}
+        $width={width}
+        onClick={onClick}>
+            {title || "Button"}
+        </StyledButton>
+    )
+}
+
+export default Button;
